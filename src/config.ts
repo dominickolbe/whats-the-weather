89d1a7370ad897cc107fad404e8054754e3376af
Dominic Kolbe <kolbedominic@gmail.com>
2021-08-25 11:42:12 +0100

    fix: get version for production

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,3 @@
 export const APP_NAME = "whats-the-weather";
-export const APP_VERSION = process.env.npm_package_version ?? "unknown";
+export const APP_VERSION =
+  (process.env.APP_VERSION || process.env.npm_package_version) ?? "unknown";
